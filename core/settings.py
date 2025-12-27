740dbd1d8fe85833629f16d75a64228e0951812b
Anaidison Regis da Silva <anaidison.silva@ufv.br>
2025-12-27 15:05:26 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -131,8 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# OBRIGATÓRIO PARA PRODUÇÃO / COLLECTSTATIC
+STATIC_ROOT = BASE_DIR / "staticfiles"
+"static"]
 
 
 # Default primary key field type
